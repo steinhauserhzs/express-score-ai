a25c28a0bed59cadd2ade48f5e5868e87f0641ed
steinhauserhzs <131223735+steinhauserhzs@users.noreply.github.com>
2025-11-12 12:31:09 -0300

    Align bootstrap flow with Laravel 11 style

--- public/index.php
+++ public/index.php
@@ -1,17 +1,13 @@
 <?php
 
+use Illuminate\Http\Request;
+
 define('LARAVEL_START', microtime(true));
 
 require __DIR__.'/../vendor/autoload.php';
 
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-
-$response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
-);
-
-$response->send();
-
-$kernel->terminate($request, $response);
+$app->handleRequest(
+    request: Request::capture()
+)->send();
